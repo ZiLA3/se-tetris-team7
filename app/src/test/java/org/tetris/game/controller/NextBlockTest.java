231c37ab86e4260c2cd60f2b2b2fa6a36e21a040
ZiLA3 <93520847+ZiLA3@users.noreply.github.com>
2025-09-30 10:38:30 +0900

    feat: NextBlockTest

---
+++ app/src/test/java/org/tetris/game/controller/NextBlockTest.java
@@ -0,0 +1,37 @@
+package org.tetris.game.controller;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.extension.Point;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.tetris.game.model.Block;
+
+public class NextBlockTest {
+    // 테스트를 위한 블럭 리스트
+    private ArrayList<Block> blockList;
+
+    @Before
+    public void setUp() {
+        blockList = new ArrayList<Block>();
+        // blockList.add(new Block(...)); // 필요한 블록들을 추가
+        blockList.add(new Block(new int[][] { {1, 1, 1}, {0, 1, 0} }, new Point(1, 1))); // T 블록 예시
+
+    }
+
+    @Test
+    public void testNextBlockController() {
+        NextBlockController controller = new NextBlockController(blockList, 3);
+
+        // 초기 블럭 큐 상태 확인
+        assertNotNull(controller.getBlock());
+        assertNotNull(controller.getBlock());
+        assertNotNull(controller.getBlock());
+
+        // 블럭이 소진된 후 새로운 블럭이 생성되는지 확인
+        assertNotNull(controller.getBlock());
+    }
+}
